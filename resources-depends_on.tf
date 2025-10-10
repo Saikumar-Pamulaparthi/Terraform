4f0b69b02ab379f8d89440c01ee517573965195f
unknown <pskc009@gmail.com>
2025-10-10 16:31:57 +0530

    creating multiple resources one by one by using depends_on

---
+++ resources-depends_on.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "Dev" {
+    ami = "ami-052064a798f08f0d3"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "Practice"
+    }
+}
+
+resource "aws_s3_bucket" "Bucket" {
+    bucket = "my-first-bucket-resource-terraform"
+    acl = "private"
+    depends_on = [ aws_instance.Dev ]
+  
+}
