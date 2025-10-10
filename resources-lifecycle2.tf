627dcc000b570cadd3041bba56d83a56c5a42967
unknown <pskc009@gmail.com>
2025-10-10 17:20:51 +0530

    lifecycle rule2 for it will ignore manually changes on resources, console level

---
+++ resources-lifecycle2.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "Dev" {
+    ami = "ami-052064a798f08f0d3"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "Practice"
+    }
+    lifecycle {
+      ignore_changes = all
+    }
+}
